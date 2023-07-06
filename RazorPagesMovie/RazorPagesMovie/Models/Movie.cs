85d587af58302851c444df65cb2998adb573b4cf
Janalisa Waugh <iamjanalisa@gmail.com>
2023-07-06 09:35:04 -0500

    started web app with razor tutorial

---
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {   //primary key
+        public int Id { get; set; }
+        //? after string means that the property is nullable
+        public string? Title { get; set; }
+        //specifies that time info isn't required by the user or displayed
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
